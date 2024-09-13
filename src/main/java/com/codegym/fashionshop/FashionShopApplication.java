f2d1e5c24b5716a75ab198cb8fcd5d2bbb2fb8c6
vankhang49 <dvkhang49@gmail.com>
2024-09-13 15:19:58 +0700

    KhangDV

--- src/main/java/com/codegym/fashionshop/FashionShopApplication.java
+++ src/main/java/com/codegym/fashionshop/FashionShopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.codegym.fashionshop.repository")
 public class FashionShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FashionShopApplication.class, args);
